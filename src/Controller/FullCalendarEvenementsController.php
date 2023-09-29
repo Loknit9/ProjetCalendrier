75574ec917d9b4682b7dd21c623a57e66e3edf80
Laurence Delforge <laurencedelforge@hotmail.com>
2023-09-29 16:48:58 +0200

    json

--- src/Controller/FullCalendarEvenementsController.php
+++ src/Controller/FullCalendarEvenementsController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FullCalendarEvenementsController extends AbstractController
 {
-    #[Route('/full/calendar/evenements', name: 'app_full_calendar_evenements')]
-    public function index(): Response
+    #[Route('/full/calendar/evenements', name:'afficher_calendrier')]
+    public function afficherCalendrierUser(SerializerInterface $serializer): Response
     {
-        return $this->render('full_calendar_evenements/index.html.twig', [
-            'controller_name' => 'FullCalendarEvenementsController',
-        ]);
+
+        //obtenir tous les evts  pour le user qui est connecté
+        
+        $user = $this->getUser();
+        $evenements = $user->getEvenements();
+        //foreach ($evenements as $evenement){
+           // dump($evenement);
+        //}
+
+        $evenementsJson = $serializer->serialize($evenements, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES=>['user']]);
+            dd($evenementsJson);
+
+
+        return $this->render('full_calendar_evenements/afficher_calendrier_user.html.twig');
     }
+
 }
